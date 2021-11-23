057597aa7c624c500e50bfc23ed069deed9aaebb
tristan-sch <89381555+tristan-sch@users.noreply.github.com>
2021-11-23 16:50:47 +0000

    saving project

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+const about = () => {
+    return (
+        <div>
+            
+            <head> 
+                <title>About</title>
+            </head>
+        
+            <h1>About</h1>
+
+        </div>
+    )
+}
+
+export default about
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    // Have to have a single parent element but does not have to be a div
+    <div>
+
+      <head> 
+        <title>Weather App with Next.js</title>
+        <meta name='keywords' content='weather, Iceland' />
+      </head>
+    
+
+      <h1>Weather App with Next.js</h1>
+    </div>
+  )
+}
