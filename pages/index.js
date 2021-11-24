a21f055e1fd8460e75cb71d6fba52d62b2829d70
tristan-sch <89381555+tristan-sch@users.noreply.github.com>
2021-11-24 20:06:33 +0000

    added dropdown

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import SelectSearch from 'react-select-search';
+import fuzzySearch from "../components/fuzzySearch"
 
 export default function Home({ stations }) {
-  console.log(stations)
+
+  
   return (
     // Have to have a single parent element but does not have to be a div
     <div>
@@ -11,28 +14,58 @@ export default function Home({ stations }) {
         <meta name='keywords' content='weather, Iceland' />
       </head>
 
-      <h1>Select the city</h1>
+      <h1>Selet the city</h1>
+
+      <SelectSearch
+        options={[]}
+        value="15997"
+        getOptions={() => {
+            return new Promise((resolve, reject) => {
+                fetch(`http://apis.is/weather/getAvailableStations`)
+                    .then(response => response.json())
+                    .then(({ results }) => {
+                        resolve(results.map(({ id, name }) => ({ value: id, name: name })))
+                    })
+                    .catch(reject);
+            });
+        }}
+        search
+        filterOptions={fuzzySearch}
+        emptyMessage="Not found"
+        placeholder="City"
+    />
+
+
+    {/* <SelectSearch
+        options={countries}
+        search
+        filterOptions={fuzzySearch}
+        emptyMessage="Not found"
+        placeholder="Select your country"
+    /> */}
 
 
 
-      {stations.map((station) => (
-        <h3>{station.name}</h3>
-      ))}
-      
+    
+
+     
+
     </div>
   )
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch ('http://apis.is/weather/getAvailableStations')
-  const data = await res.json();
-  const stations = data.results
-  // const stations = await res.json()
+// export const getStaticProps = async () => {
+//   const res = await fetch ('http://apis.is/weather/getAvailableStations')
+//   const data = await res.json();
+//   const stations = data.results
 
-  return {
-    props: {
-      stations
-    }
-  }
+  
+//   // const stations = await res.json()
 
-}
+//   return {
+//     props: {
+//       stations
+//     }
+//   }
+
+// }
